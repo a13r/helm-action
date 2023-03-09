6249f2758a8ee2b854225cb501134f0ee9302380
Andreas Bruckner <andreas@bruckner.cc>
2023-03-09 14:37:25 +0100

    feat: add debug flag

--- index.js
+++ index.js
@@ -199,6 +199,7 @@ async function run() {
     const dryRun = core.getInput("dry-run");
     const secrets = getSecrets(core.getInput("secrets"));
     const atomic = getInput("atomic") || true;
+    const debug = getInput("debug") || false;
     const helmVersion = getInput("helm-version") || fallbackVersion;
     const helm = "helm";
 
@@ -259,6 +260,10 @@ async function run() {
     // If true upgrade process rolls back changes made in case of failed upgrade.
     if (atomic === true) {
       args.push("--atomic");
+    }
+
+    if (debug) {
+      args.push("--debug");
     }
 
     // Setup necessary files.
